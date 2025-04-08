357a92749f56e8daa81843ef003ab47fa51593eb
Petr Skoda <petr@mutms.org>
2025-04-08 09:34:33 +0200

    Tidy up version file

--- version.php
+++ version.php
@@ -11,12 +11,13 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025040700;
-$plugin->requires = 2024100703;
+/** @var stdClass $plugin */
 $plugin->component = 'tool_mutenancy';
-$plugin->maturity = MATURITY_ALPHA;
+$plugin->version   = 2025040700;
+$plugin->requires  = 2024100703;
+$plugin->maturity  = MATURITY_ALPHA;
 $plugin->supported = [405, 405];
-$plugin->release = 'mu-4.5.3-01';
+$plugin->release   = 'mu-4.5.3-01';
 
 $plugin->dependencies = [
     'tool_mulib' => 2025040700,
