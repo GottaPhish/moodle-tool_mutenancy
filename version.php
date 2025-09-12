612501730275212d7fce46f1ede506bd00dd3f3a
Petr Skoda <petr@mutms.org>
2025-09-12 09:26:20 +0200

    Add event for user tenant allocation changes

--- version.php
+++ version.php
@@ -28,7 +28,7 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_mutenancy';
-$plugin->version = 2025083145;
+$plugin->version = 2025083145.01;
 $plugin->requires = 2024100705;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->supported = [405, 405];
