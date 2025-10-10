be692f4dfc898c5d3338dcd8f8818cf779d02fb7
Petr Skoda <petr@mutms.org>
2025-10-10 17:07:15 +0200

    Bump release

--- version.php
+++ version.php
@@ -33,7 +33,7 @@ $plugin->requires = 2025092600;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->supported = [501, 501];
 $plugin->incompatible = 502;
-$plugin->release = 'mu-5.1.0-01';
+$plugin->release = 'mu-5.1.0-01+';
 
 $plugin->dependencies = [
     'tool_mulib' => 2025100650,
